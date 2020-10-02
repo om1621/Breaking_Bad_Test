cf74c8b9a20270ce3bdaf76754be05123fa7b310
om1621 <onkartelange2000@gmail.com>
2020-10-02 20:32:33 +0530

    trying with build

---
+++ client/build/precache-manifest.9ba0a7260c86a5dab659fbfeeb03b094.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "129980d0696f0c860cd4e89ce97c4c4a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cbb881d813c47d5643f5",
+    "url": "/static/css/main.1e60c533.chunk.css"
+  },
+  {
+    "revision": "da74b31edbab25c70503",
+    "url": "/static/js/2.863b8b6e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.863b8b6e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cbb881d813c47d5643f5",
+    "url": "/static/js/main.ec83c3a7.chunk.js"
+  },
+  {
+    "revision": "1fa22a168f4e7d5c1c50",
+    "url": "/static/js/runtime-main.f454de49.js"
+  },
+  {
+    "revision": "1be2d3a0dc2064fe215cebb23799d503",
+    "url": "/static/media/bg.1be2d3a0.jpg"
+  },
+  {
+    "revision": "235b9b3ed13f64542cb0792e6ae7b5c4",
+    "url": "/static/media/loading.235b9b3e.gif"
+  },
+  {
+    "revision": "3795f66d4a869a7faeb4b425446daf33",
+    "url": "/static/media/logo.3795f66d.png"
+  }
+]);
